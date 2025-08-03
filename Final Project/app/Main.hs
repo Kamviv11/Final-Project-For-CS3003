b689ed534bcb9507c9a1f77cdeb2c53b15254fde
Sai Vivek Kambam <kamviv11@gmail.com>
2025-08-03 17:03:52 -0400

    Update Main.hs

--- Final Project/app/Main.hs
+++ Final Project/app/Main.hs
@@ -2,6 +2,17 @@ module Main where
 import System.IO (readFile)
 import System.Random (getStdGen, randomR)
 
+-- A function to get a random element from a list
+-- It operates within the IO monad.
+getRandomElement :: [a] -> IO a
+getRandomElement xs = do
+    -- Get the length of the list to determine the index range
+    let len = length xs
+    -- Generate a random index. Note: list indices are 0-based.
+    index <- randomRIO (0, len - 1)
+    -- Return the element at the random index
+    return String(xs !! index)
+
 main :: IO ()
 main = do
     let file = "words.txt"
