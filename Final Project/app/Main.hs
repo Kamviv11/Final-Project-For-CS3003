adb5cbd8c4e608027bf5b3737a2429cf00cf18a4
Sai Vivek Kambam <kamviv11@gmail.com>
2025-08-04 14:31:09 -0400

    Update Main.hs
    Final fix hopefully

--- Final Project/app/Main.hs
+++ Final Project/app/Main.hs
@@ -1,11 +1,11 @@
 module Main where
-import System.IO ()
+import System.IO
 import System.Random
 import Distribution.SPDX (LicenseId(NullBSD))
 import Data.List
 import Text.Parsec (State(State))
 
-data State = Correct | Misplaced | Incorrect
+data State = Correct | Misplaced | Incorrect deriving (Eq, Show)
 
 -- A function to get a random element from a list
 -- It operates within the IO monad.
@@ -18,29 +18,49 @@ getRandomElement xs = do
     -- Return the element at the random index
     return (xs !! index)
 
-isLetterCorrect :: String -> String -> Int -> String
-isLetterCorrect answer (x:xs) indexOfChar = do
-    let allIndices = elemIndices x answer
-    if null allIndices 
-        then x + "Is Incorrect" + isLetterCorrect answer xs (indexOfChar + 1) 
-        else helper2 indexOfChar allIndices + isLetterCorrect answer xs (indexOfChar + 1)
+isGuessCorrect :: String -> String -> [State]
+isGuessCorrect answer guess = zipWith helper2 answer guess
     where
-        helper2 :: Int -> [Int] -> String
-        helper2 _ [] = x + "is Misplaced"
-        helper2 x (y:ys) = if x == y then x + "is Correct" else helper2 x ys
+        helper2 :: Char -> Char -> State
+        helper2 answerChar guessChar
+            | answerChar == guessChar = Correct
+            | guessChar `elem` answer   = Misplaced
+            | otherwise               = Incorrect
 
-GameLogic :: Int -> String -> Nothing
-GameLogic x answer = do
-    putStr "Enter your guess (5 letters)"
-    hFlush stdout --input buffer
-    guess <- getLine
-    let isLetterCorrect answer guess 0
-    putStr "Guess " + x + "/6"
+displayResult :: String -> [State] -> IO ()
+displayResult guess states = do
+    let formattedResult = zipWith formatLetter guess states
+    putStrLn ("Result:  " ++ unwords formattedResult ++ "\n")
+    where
+      formatLetter :: Char -> State -> String
+      formatLetter c Correct   = "🟩" ++ [c] ++ "🟩"
+      formatLetter c Misplaced = "🟨" ++ [c] ++ "🟨"
+      formatLetter c Incorrect = "🟥" ++ [c] ++ "🟥"
 
 
+GameLogic :: String -> Int -> IO()
+GameLogic answer guess_NUM = 
+    if guess_NUM == 6 then 
+        putStrLn ("You lost the game! The word was " ++ answer)
+    else do
+        putStrLn "Enter your guess (5 letters)"
+        hFlush stdout --input buffer
+        guess <- getLine
+
+        if length guess /= length answer then do
+            putStrLn "Invalid guess length. Please try again."
+            GameLogic answer guess_NUM
+        else if guess == answer then do
+            displayResult guess (isGuessCorrect answer guess)
+            putStrLn "Congratulations, you won!"
+        else do
+            displayResult guess (isGuessCorrect answer guess)
+            putStrLn ("This is Guess " ++ show(guess_NUM) ++ "/6")
+            GameLogic answer (guess_NUM + 1)
 
 main :: IO ()
 main = do
+    putStrLn "--- Welcome to Haskell Wordle ---"
     --Takes in the file contents of "words.txt" and puts it into fileContent
     let file = "words.txt"
     fileContent <- readFile file
@@ -48,13 +68,6 @@ main = do
     let wordList = lines fileContent
     --gets a random word and puts it into randomWord
     randomWord <- getRandomElement wordList
-    putStrLn randomWord
-
-    let current_guesses = 0
-    let MAX_guesses = 6
-    --Getting player input
-    putStr "Enter your guess (5 letters)"
-    hFlush stdout --input buffer
-    guess <- getLine
+    GameLogic randomWord 0
 
     
